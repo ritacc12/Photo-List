65e077007e414165c78a201180f8e7d548b5d2e3
ritacc12 <rita20920@gmail.com>
2023-12-02 15:08:40 +0800

    import search & picture function

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Search from "../components/Search";
+import Picture from "../components/Picture";
 
 const Homepage = () => {
+  let [data, setData] = useState(null);
+  let [input, setInput] = useState("");
+  const auth = "s3IRA9YtCwS89FkJRjIsTaUCxD8SCbAtK4uAlVyACn0TetZpJWB28ZWe";
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+
+  const search = async (url) => {
+    let result = await axios.get(url, {
+      headers: { Authorization: auth },
+    });
+    setData(result.data.photos);
+  };
+
+  useEffect(() => {
+    search(initialURL);
+  }, []);
+
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search />
+      <Search
+        search={() => {
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} key={d.id} />;
+          })}
+      </div>
     </div>
   );
 };
