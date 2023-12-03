d5a240cac18c145a6dec8f3bd1a150e0f309fd6b
ritacc12 <rita20920@gmail.com>
2023-12-03 18:39:32 +0800

    revised the style of homepage

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const Picture = ({ data }) => {
   return (
     <div className="picture">
-      <p>{data.photographer}</p>
+      <p className="photographer">{data.photographer}</p>
       <div className="imageContainer">
         <img src={data.src.large} alt="" />
       </div>
-      <p>
-        在此下載圖片:{" "}
-        <a target="_blank" href={data.src.large} rel="noreferrer">
-          按一下
-        </a>
-      </p>
+      <div className="download">
+        <p>
+          在此下載圖片:{" "}
+          <a target="_blank" href={data.src.large} rel="noreferrer">
+            按一下
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
